3aa411a42ab240b5103d312aa75d72a6f0e9b9fd
jpetersson86 <jpetersson86@cc8a84ad-3c34-5482-6608-053f25543658>
2010-10-28 12:02:02 +0000

    Shader files?

--- WindowHandler.h
+++ WindowHandler.h
@@ -8,8 +8,8 @@
 #include <stdio.h>
 #include <sys/stat.h>
 
-
-
+#include "GLstuff/GLObject.h"
+#include "Geometry/Mesh.h"
 using namespace std;
 
 #ifndef WindowHandler_H
@@ -25,7 +25,43 @@ public:
     void idle();
     void init();
     void reshape(int width, int height);
+    void Render();
+    bool Load(Mesh *mesh); // false return on error
+    bool ReadHeader(std::istream &is);
+    bool ReadData(std::istream &is);
 
+    static Vector3<unsigned int> ReadTri(std::istream &is);
+
+    template <class MeshType>
+    MeshType * WindowHandler::AddMesh()
+    {
+    /*  String filename = path.AfterLast('/');
+      if (filename == path) // If we're on Windows
+        filename = path.AfterLast('\\');
+      String suffix = path.AfterLast('.');
+
+      if (suffix == _T("obj")) {*/
+        // Create new mesh
+        MeshType * mesh = new MeshType();
+        mesh->SetName("sphereSmall");
+
+        // Load mesh and add to geometry list
+        std::ifstream infile;
+        ObjIO objIO;
+        infile.open("Mesh/sphereSmall.obj/");
+        objIO.Load(mesh, infile);
+        mesh->Initialize();
+
+        // Add mesh to scene
+        AddUniqueObject(mesh);
+
+        return mesh;
+      /*}
+      else
+        std::cerr << "Error: File type not supported" << std::endl;*/
+
+      return NULL;
+    }
 
 private:
 
@@ -34,6 +70,11 @@ private:
     int textureSize;
 
     Shader *shader;
+
+    struct LoadData{
+    std::vector<Vector3<float> > verts;
+    std::vector<Vector3<unsigned int> > tris;
+  } loadData;
 
 };
 #endif
