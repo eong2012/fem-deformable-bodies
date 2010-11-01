b8c9ac7b4d9c7926cee2ffa3c7e83dc6929c04b0
jpetersson86 <jpetersson86@cc8a84ad-3c34-5482-6608-053f25543658>
2010-11-01 09:40:08 +0000

    Shaders

---
+++ trunk/Shader/fragmentPhongShader.glsl
@@ -0,0 +1,25 @@
+varying vec4 v_color;
+varying vec2 v_texCoord;
+
+varying vec3 normal;
+varying vec3 vertex;
+void main (void)
+{
+	vec3 n,lightDir;
+	vec4 diffuse, ambient;
+	float nDotLight;
+
+	n = normalize(normal);
+    n=n;
+    lightDir = normalize(vec3(-1.0,1.0,1.0));
+	nDotLight = max(dot(n,lightDir),0.0);
+
+
+	ambient = vec4(0.2,0.2,0.2,1.0);
+	diffuse = vec4(0.8,0.8,0.8,1.0)*nDotLight;
+	diffuse = clamp(diffuse,0.0,1.0);
+
+	gl_FragColor = ambient + diffuse;
+
+}
+
