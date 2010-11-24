2680a3ca64fbcacc579df209c74b32986e36ecb4
jpetersson86 <jpetersson86@cc8a84ad-3c34-5482-6608-053f25543658>
2010-11-24 15:46:52 +0000

    Woooooooo

--- trunk/mesh/VolumeGenerator.cpp
+++ trunk/mesh/VolumeGenerator.cpp
@@ -12,56 +12,55 @@ VolumeGenerator::VolumeGenerator() {
 
 void VolumeGenerator::generateVolume() {
 
-	//arma::Mat<double> vertex1,vertex2,vertex3,vertex4, vertex5, vertex6, vertex7, vertex8;
+//	arma::Mat<double> vertex1,vertex2,vertex3,vertex4, vertex5, vertex6, vertex7, vertex8;
 
     vector<arma::Mat<double> > tempVertices;
     vector<unsigned int> indices;
 
-	tempVertices = meshReader->readVertices("P.node");
-	indices = meshReader->readTetras("P.ele");
+	tempVertices = meshReader->readVertices("Bunny.node");
+	indices = meshReader->readTetras("Bunny.ele");
     arma::Mat<double> vertex1,vertex2,vertex3,vertex4;
 
-	for(int i = 0; i <indices.size()/4-4; i++){
+    cout << indices.size() << endl;
+    cout << tempVertices.size() << endl;
+	for(int i = 0; i <indices.size()/4; i++){
 
-        //cout << "krash?" << endl;
-        //cout << tempVertices.at(indices.at(i*4+4)-1) << endl;
-        vertex1 = tempVertices.at(indices.at(i*4)-1)/10;
-        vertex2 = tempVertices.at(indices.at(i*4+1)-1)/10;
-        vertex3 = tempVertices.at(indices.at(i*4+2)-1)/10;
-        vertex4 = tempVertices.at(indices.at(i*4+3)-1)/10;
+        vertex1 = tempVertices.at(indices.at(i*4));
+        vertex2 = tempVertices.at(indices.at(i*4+1));
+        vertex3 = tempVertices.at(indices.at(i*4+2));
+        vertex4 = tempVertices.at(indices.at(i*4+3));
 
         createTetra(vertex1,vertex2, vertex3, vertex4);
-
     }
 
 
-  //  createTetra(vertex1,vertex2, vertex3, vertex4);
-
-	// Skapa ett gäng vertexpunkter
+//    createTetra(vertex1,vertex2, vertex3, vertex4);
+//
+//	 Skapa ett gäng vertexpunkter
 //	vertex1 = vertices.at(indices.at(0));
 //	vertex3 = vertices.at(indices.at(1));
 //	vertex8 = vertices.at(indices.at(2));
 //	vertex4 = vertices.at(indices.at(3));
 
 
-//    vertex1 <<  0.5f <<  0.5f <<  0.5f;
-//	vertex2 << -0.5f <<  0.5f << -0.5f;
-//    vertex3 <<  0.5f << -0.5f << -0.5f;
-//    vertex4 << -0.5f << -0.5f <<  0.5f;
-//    vertex5 <<  0.5f <<  0.5f << -0.5f;
-//    vertex6 << -0.5f <<  0.5f <<  0.5f;
-//    vertex7 << -0.5f << -0.5f << -0.5f;
-//    vertex8 <<  0.5f << -0.5f <<  0.5f;
-
-    //Tetror med negativ volym
-   // createTetra(vertex1,vertex3, vertex8, vertex4);
-
-
-    //Tetror med positiv volym
-   // createTetra(vertex1,vertex2, vertex3, vertex4); //Tetra 1
-    //createTetra(vertex1,vertex2, vertex4, vertex6); //Tetra 4
-    //createTetra(vertex2,vertex3, vertex4, vertex7); //Tetra 5
-    //createTetra(vertex1,vertex2, vertex5, vertex3);
+//    vertex1 <<  0.05f <<  0.05f <<  0.05f;
+//	vertex2 << -0.05f <<  0.05f << -0.05f;
+//    vertex3 <<  0.05f << -0.05f << -0.05f;
+//    vertex4 << -0.05f << -0.05f <<  0.05f;
+//    vertex5 <<  0.05f <<  0.05f << -0.05f;
+//    vertex6 << -0.05f <<  0.05f <<  0.05f;
+//    vertex7 << -0.05f << -0.05f << -0.05f;
+//    vertex8 <<  0.05f << -0.05f <<  0.05f;
+//
+//
+//    createTetra(vertex1,vertex3, vertex8, vertex4);
+//
+//
+//
+//    createTetra(vertex1,vertex2, vertex3, vertex4); //Tetra 1
+//    createTetra(vertex1,vertex2, vertex4, vertex6); //Tetra 4
+//    createTetra(vertex2,vertex3, vertex4, vertex7); //Tetra 5
+//    createTetra(vertex1,vertex2, vertex5, vertex3);
 
 
 }
@@ -71,7 +70,7 @@ void VolumeGenerator::createTetra(arma::Mat<double> v1, arma::Mat<double> v2, ar
     //Kolla efter positivt volym
     arma::Mat<double> Volmat;
     Volmat = join_rows(arma::ones(4,1),join_cols(join_cols(join_cols(v1,v2),v3),v4));
-    double V = 1; //det(Volmat)/6.0;
+    double V =  det(Volmat)/6.0;
     if(V > 0){
         //Skapa tetras
         arma::Mat<double> t = arma::zeros(1,3);
@@ -87,8 +86,7 @@ void VolumeGenerator::createTetra(arma::Mat<double> v1, arma::Mat<double> v2, ar
         tetrahedMesh->AddTetrahedron(vertices);
     }
     else
-        std::cout << V << endl;
-
+       createTetra(v2, v3, v4, v1);
 
     }
 
