3cd2fac6969bd1658cc2b8f71761178b55f8e5ee
fredrik.ludde.lundell@gmail.com <fredrik.ludde.lundell@gmail.com@cc8a84ad-3c34-5482-6608-053f25543658>
2010-12-09 12:11:41 +0000

    sd

--- Solver/Solver.h
+++ Solver/Solver.h
@@ -13,29 +13,35 @@ public:
     void constructKe(TetrahedMesh *mesh);
     void calcNewPosition(TetrahedMesh *mesh, arma::Mat<double> Fxt);
 	void tetrahedronAssemble(arma::Mat<double> &K ,arma::Mat<double> k, int i, int j, int m, int n);
-	void setStaticState() {update = true;}
+	arma::Mat<double> calculateB(arma::Mat<double> x1,arma::Mat<double> x2, arma::Mat<double> x3, arma::Mat<double> x4);
+	arma::Mat<double> Solver::calculateD(float E,float NU);
+
+	arma::Mat<double> calculateRotation(arma::Mat<double> X, arma::Mat<double> X1);
+	arma::Mat<double> findRotation(TetrahedMesh *mesh,unsigned int theInd);
+	arma::Mat<double> TetrahedronElementStiffness(float E,float NU,arma::Mat<double> x1,arma::Mat<double> x2, arma::Mat<double> x3, arma::Mat<double> x4);
+	arma::Mat<double> calculateStress(TetrahedMesh *mesh,unsigned int theInd, float E,float NU, arma::Mat<double> R);
+	arma::Mat<double> calculateLargestEIG(arma::Mat<double> stresstensor);
+	arma::Mat<double> crackIT(TetrahedMesh *mesh, unsigned int theInd, arma::Mat<double> stresstensor);
+	void changeFracture();
+
 	void planeCollisionDetection(arma::Mat<double> X);
 	void planeCollisionHandler(unsigned int forceIndex);
 	void constructMe(TetrahedMesh *mesh);
-	arma::Mat<double> TetrahedronElementStiffness(float E,float NU,arma::Mat<double> x1,arma::Mat<double> x2, arma::Mat<double> x3, arma::Mat<double> x4);
+	
+	void setParameter(float mass, float fracture);
 
 
 private:
 
 	ConjugateGradient *conjugateGradient;
     vector<arma::Mat<double> > mKMatrices;
-	arma::Mat<double> K;
-	arma::Mat<double> M;
-	arma::Mat<double>  *xOVer1;
-	arma::Mat<double>  *xOVer2;
-	arma::Mat<double>  *vOVer1;
-	arma::Mat<double>  *vOVer2;
-	bool check,update;
-
+	vector<arma::Mat<double> > mBMatrices;
+	
 	arma::Mat<double> Vpre;
 	arma::Mat<double> Xpre;
 	arma::Mat<double> grav;
 	vector<arma::Mat<double> >  xOrgin;
+	
 	arma::Mat<double> normal;
 	int nrOfNodes;
 	arma::Mat<double> ForcePrev;
@@ -43,9 +49,23 @@ private:
 	arma::Mat<double> v;
 	arma::Mat<double> X;
 	arma::Mat<double> collisionForce;
+	
+	arma::Mat<double> K;
+	arma::Mat<double> C;
+	arma::Mat<double> M;
+
+	arma::Mat<double> stress;
+	
 	double dt;
 	double mass;
 	double density;
+
+	double E;
+	double alpha, beta;
+	double vn;
+
+	bool allowFracture;
+	double FractureThresh;
 
 	vector<Vertex>* mOriginalPos;
 	arma::Mat<double> localVpre;
